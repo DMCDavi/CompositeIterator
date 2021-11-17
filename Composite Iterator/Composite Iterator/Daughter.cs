67f44617edc4d2ef47d85aaf6c99401a325d7940
guilhermevergne <51097229+guilhermevergne@users.noreply.github.com>
2021-11-17 12:28:50 -0300

    Add files via upload

---
+++ Composite Iterator/Composite Iterator/Daughter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite_Iterator
+{
+    class Daughter : Person
+    {
+        public Daughter(bool artist, string name)
+        {
+            this.artist = artist;
+            this.name = name;
+        }
+        public override int attribute()
+        {
+            return 69;
+        }
+        public override void print()
+        {
+            if(artist) Console.WriteLine($"{name} é uma filha artista");
+            else Console.WriteLine($"{name} é uma filha não artista");
+        }
+        public override bool isArtist()
+        {
+            return artist;
+        }
+    }
+}
---
+++ Composite Iterator/Composite Iterator/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite_Iterator
+{
+    public abstract class Person
+    {
+        public string name;
+        public bool artist;
+        public abstract int attribute();
+        public abstract void print();
+        public virtual void add(Person person)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual void remove(Person person)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual bool isArtist()
+        {
+            throw new NotImplementedException();
+        }
+        public virtual IEnumerator createIterator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
