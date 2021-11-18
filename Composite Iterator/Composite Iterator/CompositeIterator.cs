0ecfd59230032493d253638b0f759f1c9728fe2c
JOAO FELIPE DE ARAUJO CALDAS <41966837+jfdac11@users.noreply.github.com>
2021-11-18 10:06:32 -0300

    Add files via upload

---
+++ Composite Iterator/Composite Iterator/CompositeIterator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite_Iterator
+{
+    class CompositeIterator : IEnumerator
+    {
+        public Stack<IEnumerator> stack = new Stack<IEnumerator>();
+        public object Current => throw new NotImplementedException();
+
+        public CompositeIterator(IEnumerator iterator)
+        {
+            stack.Push(iterator);
+        }
+        /*public bool HasNext()
+        {
+            if(stack.Count == 0)
+            {
+                return false;
+            }
+            else
+            {
+                IEnumerator iterator = stack.Peek();
+                //iterator.MoveNext();
+            }
+            return true;
+
+        }*/
+        public bool MoveNext()
+        {
+            IEnumerator iterator = stack.Peek();
+            iterator.MoveNext();
+            Person person = (Person)iterator.Current;
+            if(person.GetType() == typeof(Mother))
+            {
+                stack.Push(person.createIterator());
+                return true;
+            }
+            return true;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Composite Iterator/Composite Iterator/Mother.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite_Iterator
+{
+    class Mother : Person
+    {
+        public List<Person> people = new List<Person>();
+        public IEnumerator iterator;
+        public Mother(bool artist, string name)
+        {
+            this.artist = artist;
+            this.name = name;
+        }
+        public override int attribute()
+        {
+            return 69;
+        }
+        public override void print()
+        {
+            if (artist) Console.WriteLine($"{name} é uma mãe artista");
+            else Console.WriteLine($"{name} é uma mãe não artista");
+            IEnumerator<Person> iterator = people.GetEnumerator();
+            while (iterator.MoveNext())
+            {
+                iterator.Current.print();
+                
+            }
+        }
+        public override void add(Person person)
+        {
+            people.Add(person);
+        }
+        public override void remove(Person person)
+        {
+            people.Remove(person);
+        }
+        public override IEnumerator createIterator()
+        {
+            if (iterator == null) iterator = new CompositeIterator(people.GetEnumerator());
+            return iterator;
+        }
+    }
+}
